6ba56753612784e5761e975c9890ac1efbcca0c6
Gladis <gladis@Equipo1.com>
2023-05-18 13:43:07 -0600

    Gif-Demo-App iniciando

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+function App(){
+
+const [categories, setCategories]= useState(["Dragon Ball", "One Piece"])
+
+  return(
+    <div className="ms-5 mt-5 me-5">
+      <h3>Gif Demo App</h3>
+      < hr/>
+      <input
+         placeholder="Search term"
+         type="text"
+         />
+         <button
+            className="btn btn-primary btn-sm ms-2"
+            type="button"
+            >
+              search
+            </button>
+            <br />
+            <ol>
+
+            {
+              categories.map((category) => (
+               <li key={category}>
+                {category}
+                </li>
+              ))
+            }
+            </ol>
+            </div>     
+    
+  )
+}
+export default App
