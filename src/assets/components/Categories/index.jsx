b120d7faacf67896364cb0974095a2153df2ad36
Gladis <gladis@Equipo1.com>
2023-05-23 17:15:30 -0600

    Las url

--- src/assets/components/Categories/index.jsx
+++ src/assets/components/Categories/index.jsx
@@ -1,40 +1,39 @@
-import { useState } from "react";
+import { useState } from "react"
 import CategoriesList from "./CategoriesList"
+import GifExpo from "../GifsExpo"
 
-const Categories = ({categories = [], setCategories}) => {
-    const [inputValue, setInputValue] =useState("")
+const Categories = ({categories = [], setCategories}) =>{
 
-    const handleInput = ({target}) => {
+    const [inputValue, setInputValue] = useState("")
+    const handleInput = ({target}) =>{
         setInputValue(target.value)
     }
-
-
-    const handleAddCategoryButton = () => {
+    const handleAddCategoryButtom = () =>{
         if (!categories.includes(inputValue)){
             setCategories([inputValue, ...categories])
-        setInputValue("")
+            setInputValue("")
         }
     }
-
-    return (
+    return(
         <>
-        <input
-      onChange={(e) => handleInput(e)} 
-      placeholder="Write category name"
-      type="text"
-      value={inputValue} 
-      />
-      <button
-      className="btn btn-primary btn-sm ms-2"
-      onClick={handleAddCategoryButton}
-      type="button"
-      >
+        <input 
+        onChange={(e)=> handleInput(e)}
+        placeholder="Write Category Name"
+        type="text"
+        value={inputValue} 
+        />
+        <button
+        className="btn btn-primary btn-sm ms-2 mb-1"
+        onClick={handleAddCategoryButtom}
+        type="button"
+        >
         Add
-      </button>
-      <br />
-      <CategoriesList categories={categories} />
-      </>
+        </button>
+        <br />
+        <CategoriesList categories={categories}/>
+        <hr />
+        <GifExpo categories={categories}/>
+        </>
     )
 }
-
 export default Categories
--- src/assets/components/GifsExpo/index.jsx
+++ src/assets/components/GifsExpo/index.jsx
@@ -1,32 +1,54 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
-const GifsExpo = ({categories}) => {
-    const [gifsLists, setGifsLists] = useState([])
+const GifExpo = ({categories}) =>{
+    const [gifsList, setGifsList] = useState([])
 
 
-    const getGifsList = async () => {
-        let gifArray = []
-        categories.forEach(async(category)=> {
-          const query = encodeURIComponent(category)
-          const apikey = "QdlBd7eQ8b2cKuiraj3Bx6eqsDDw646P"
-          const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q${query}limit=10`)
-          const gifsLists = await response.json()
-
-        const urlGifsList = gifsLists.data.map((gif)=> {
-            return gif.images.fixed_width.url
-        })
-        gifArray = [...gifArray, ...urlGifsList]
-
-       })
-    //    setGifsLists(...gifArray)
-
-    }
+    useEffect(
+        ()=>{
+            console.log("Me renderizo")
+            const getGifsList = async () => {
     
-getGifsList()
+                const gifs = await Promise.all(categories.map(async(category) => {
+    
+                const query = encodeURIComponent(category)
+                const apikey = "ZuHKFXydNMHzdsLofDqbNM7JZjVsLzX0"
+                const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${query}&limit=10`)
+                const gifsList = await response.json()
+                return gifsList
+            }))
+            let urlList = []
+            gifs.forEach((gif)=>{
+                const data = gif.data.map((item)=>{
+                    return item.images.fixed_width.url
+                })
+                urlList = [...urlList, ...data]
+            })
 
-    return (
-        <h4>GIFSEXPO</h4>
-    )
+            ///getGifsList()
+        setGifsList([...urlList])
+    }
+    getGifsList()
+},
+[categories]
+)
+return(
+    <>
+     <h4>Gifs Expo</h4>
+    <ol> 
+    {
+     gifsList.map((url)=>(
+        <li key={url}>
+        {url}
+        </li>
+            ))
+        }
+        </ol>
+    </>
+)
+
 }
+export default GifExpo
 
-export default GifsExpo
+
+    
