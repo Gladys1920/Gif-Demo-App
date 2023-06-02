2d5d116bfa9366268a57737e9b0e0a039ce94ed8
Gladis <gladis@Equipo1.com>
2023-06-02 00:27:30 -0600

    Eliminar

--- src/assets/components/Categories/index.jsx
+++ src/assets/components/Categories/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import CategoriesList from "./CategoriesList"
 import GifExpo from "../GifsExpo"
+import Swal from "sweetalert2"
+
 const Categories = ({categories = [], setCategories}) =>{
     const [inputValue, setInputValue] = useState("")
     const handleInput = ({target}) =>{
@@ -12,6 +14,22 @@ const Categories = ({categories = [], setCategories}) =>{
             setInputValue("")
         }
     }
+
+    const clearList = async () =>{
+        const result = await Swal.fire({
+            title:"Clear the List?",
+            text:"You won't be able to revert this!",
+            icon:"warning",
+            showCancelButton:true,
+            confirmButtonText:"Yes, clear it!",
+        });
+
+        if(result.isConfirmed){
+            localStorage.setItem("categories", JSON.stringify([]))
+            setCategories([]);
+        }
+    }
+
     return(
         <>
         <input 
@@ -27,13 +45,29 @@ const Categories = ({categories = [], setCategories}) =>{
         >
         Add
         </button>
+        <button
+        className="btn btn-danger btn-sm ms-2 mb-1"
+        onClick={clearList}
+        type="button"
+        >
+        Eliminar
+        </button>
+        
         <br />
         <CategoriesList 
         categories={categories} 
         setCategories={setCategories}/>
         <hr />
         <GifExpo categories={categories}/>
+        <br />
+        {
+        categories.length === 0 && (<h4>Add a category...</h4>)
+        }
         </>
+
+
+
+
     )
 }
 export default Categories
